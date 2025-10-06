f7d9fc42c82ccae9d489b58e0b92a79e28fed963
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-10-06 17:43:23 -0500

    solucion punto 4

--- homework/pregunta_04.py
+++ homework/pregunta_04.py
@@ -1,22 +1,24 @@
 """
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
+Calcule el promedio de `c2` por cada letra de la `c1` del archivo
+`tbl0.tsv`.
+
+Rta/
+c1
+A    4.625000
+B    5.142857
+C    5.400000
+D    3.833333
+E    4.785714
+Name: c2, dtype: float64
 """
 
+import pandas as pd
 
 def pregunta_04():
-    """
-    Calcule el promedio de `c2` por cada letra de la `c1` del archivo
-    `tbl0.tsv`.
+    table0 = pd.read_csv('files/input/tbl0.tsv', sep='\t')
 
-    Rta/
-    c1
-    A    4.625000
-    B    5.142857
-    C    5.400000
-    D    3.833333
-    E    4.785714
-    Name: c2, dtype: float64
-    """
+    return table0.groupby('c1')['c2'].agg('mean')
+
+print(pregunta_04())
+
+

d8cc9367339e44fcde3ba454c9e790b2b0d530e9
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-10-06 17:31:25 -0500

    solucion punto 2

--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -5,7 +5,7 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 librerias de pandas para resolver las preguntas.
 """
 
-
+import pandas as pd
 def pregunta_02():
     """
     ¿Cuál es la cantidad de columnas en la tabla `tbl0.tsv`?
@@ -14,3 +14,7 @@ def pregunta_02():
     4
 
     """
+
+    table0 = pd.read_csv('files/input/tbl0.tsv', sep='\t')
+
+    return table0.shape[1]

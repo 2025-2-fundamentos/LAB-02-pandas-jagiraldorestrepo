a42c60f696003a28914f1671a26d122e5dd1e12b
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-10-06 17:58:41 -0500

    solucion punto 7

--- homework/pregunta_07.py
+++ homework/pregunta_07.py
@@ -1,22 +1,21 @@
 """
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
+Calcule la suma de la `c2` por cada letra de la `c1` del archivo
+`tbl0.tsv`.
+
+Rta/
+c1
+A    37
+B    36
+C    27
+D    23
+E    67
+Name: c2, dtype: int64
 """
-
-
+import pandas as pd
 def pregunta_07():
-    """
-    Calcule la suma de la `c2` por cada letra de la `c1` del archivo
-    `tbl0.tsv`.
+    table0 = pd.read_csv('files/input/tbl0.tsv', sep='\t')
 
-    Rta/
-    c1
-    A    37
-    B    36
-    C    27
-    D    23
-    E    67
-    Name: c2, dtype: int64
-    """
+    return table0.groupby('c1')['c2'].agg('sum')
+
+print(pregunta_07())
+

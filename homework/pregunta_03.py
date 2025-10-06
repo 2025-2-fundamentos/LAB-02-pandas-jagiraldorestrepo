bfa0f7b97afabac6277183d43330306e083211fd
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-10-06 17:38:34 -0500

    solucion punto 3

--- homework/pregunta_03.py
+++ homework/pregunta_03.py
@@ -1,23 +1,26 @@
 """
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
+¿Cuál es la cantidad de registros por cada letra de la columna `c1` del
+archivo `tbl0.tsv`?
+
+Rta/
+c1
+A     8
+B     7
+C     5
+D     6
+E    14
+Name: count, dtype: int64
+
+c0	c1	c2	c3
+0	E	1	1999-02-28
+1	A	2	1999-10-28
 """
-
-
+import pandas as pd
 def pregunta_03():
-    """
-    ¿Cuál es la cantidad de registros por cada letra de la columna `c1` del
-    archivo `tbl0.tsv`?
+    table0 = pd.read_csv('files/input/tbl0.tsv', sep='\t')
+    
+    return table0.value_counts('c1').sort_index()
 
-    Rta/
-    c1
-    A     8
-    B     7
-    C     5
-    D     6
-    E    14
-    Name: count, dtype: int64
 
-    """
+
+

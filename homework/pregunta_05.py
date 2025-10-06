4599fe07ab53a91e742f077ee7630b5f47f97261
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-10-06 17:45:12 -0500

    solucion punto 5

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -1,22 +1,22 @@
 """
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
+Calcule el valor máximo de `c2` por cada letra en la columna `c1` del
+archivo `tbl0.tsv`.
+
+Rta/
+c1
+A    9
+B    9
+C    9
+D    7
+E    9
+Name: c2, dtype: int64
 """
-
-
+import pandas as pd
 def pregunta_05():
-    """
-    Calcule el valor máximo de `c2` por cada letra en la columna `c1` del
-    archivo `tbl0.tsv`.
+    table0 = pd.read_csv('files/input/tbl0.tsv', sep='\t')
 
-    Rta/
-    c1
-    A    9
-    B    9
-    C    9
-    D    7
-    E    9
-    Name: c2, dtype: int64
-    """
+    return table0.groupby('c1')['c2'].agg('max')
+
+
+
+print(pregunta_05())

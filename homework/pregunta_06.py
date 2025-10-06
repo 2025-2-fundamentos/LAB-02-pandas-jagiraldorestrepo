8beb291136e9561e5e783fbe91e899f25cbef0da
jagiraldorestrepo <jugiraldore@unal.edu.co>
2025-10-06 17:56:51 -0500

    solucion punto 6

--- homework/pregunta_06.py
+++ homework/pregunta_06.py
@@ -1,17 +1,31 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
-"""
 
 
+"""
+Retorne una lista con los valores unicos de la columna `c4` del archivo
+`tbl1.csv` en mayusculas y ordenados alfabéticamente.
+
+Rta/
+['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+"""
+
+import pandas as pd
 def pregunta_06():
-    """
-    Retorne una lista con los valores unicos de la columna `c4` del archivo
-    `tbl1.csv` en mayusculas y ordenados alfabéticamente.
+    table1 = pd.read_csv('files/input/tbl1.tsv', sep='\t')
 
-    Rta/
-    ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    lista_unicos = table1['c4'].unique() #devuelve un array de numpy, no un str, por eso lo tengo que convertir a list
+    
 
-    """
+    #aqui yo haría tolist().sort() pero eso devuelve none
+    #porque lo unico que hace es ordenarla en el instante
+    lista_unicos = lista_unicos.tolist()
+
+    #aqui ya habia retornado con sorted(lista_unicos).upper(), pero esto
+    #es un metodo para strings, asi que hago la list comprehension
+
+    lista_unicos = [x.upper() for x in lista_unicos]
+
+    return sorted(lista_unicos)
+
+
+print(pregunta_06())
